79ad9f06461fbc47ee845290fdf002ac5b632a6d
iamanjali <iamanjalijaiswal@gmail.com>
2024-09-01 21:46:12 +0530

    Navbar and Hero Section commit

--- qtify/src/components/Carousel/Carousel.jsx
+++ qtify/src/components/Carousel/Carousel.jsx
@@ -33,30 +33,36 @@
 
 
 import React, { useRef } from 'react';
-import { Swiper, SwiperSlide} from 'swiper/react';
-import { Navigation } from 'swiper/navigation';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/bundle';
+import { Pagination } from 'swiper/modules/pagination';
 
 const Carousel = ({ items }) => {
   const swiperRef = useRef(null);
 
+  const handleSlidePrev = () => {
+    if (swiperRef.current) {
+      swiperRef.current.slidePrev();
+    }
+  };
+
+  const handleSlideNext = () => {
+    if (swiperRef.current) {
+      swiperRef.current.slideNext();
+    }
+  };
 
   return (
     <div className="carousel-container">
       <Swiper
-        modules={[Navigation]}
+        modules={[Pagination]} // Include Pagination in the modules array
         spaceBetween={10}
         slidesPerView={3}
-        navigation
-        breakpoints={{
-          640: { slidesPerView: 1 },
-          768: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
-        }}
+        pagination={{ clickable: true }} // Configure pagination options
         onSwiper={(swiper) => { swiperRef.current = swiper; }}
       >
-        {items.map(item => (
+        {items.map((item) => (
           <SwiperSlide key={item.id}>
             <div className="card-wrapper">
               {item.component}
@@ -64,11 +70,11 @@ const Carousel = ({ items }) => {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className="navigation-buttons">
-        <button className="navigation-button left" onClick={() => swiperRef.current.slidePrev()}>
+      <div className="custom-navigation">
+        <button className="navigation-button left" onClick={handleSlidePrev}>
           {/* Left navigation button icon or text */}
         </button>
-        <button className="navigation-button right" onClick={() => swiperRef.current.slideNext()}>
+        <button className="navigation-button right" onClick={handleSlideNext}>
           {/* Right navigation button icon or text */}
         </button>
       </div>
