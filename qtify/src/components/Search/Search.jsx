748efbdbe5baa01f443b7b18856da7f07b360a2f
iamanjali <iamanjalijaiswal@gmail.com>
2024-09-01 16:31:20 +0530

    Navbar and Hero Section commit

--- qtify/src/components/Search/Search.jsx
+++ qtify/src/components/Search/Search.jsx
@@ -3,7 +3,7 @@ import styles from "./Search.module.css";
 import { ReactComponent as SearchIcon } from "../../assets/search-icon.svg";
 import useAutocomplete from "@mui/base/useAutocomplete";
 import { styled } from "@mui/system";
-import { truncate } from "../../helpers/helpers";
+//import { truncate } from "../../helpers/helpers";
 import { useNavigate } from "react-router-dom";
 import { Tooltip } from "@mui/material";
 
@@ -100,7 +100,7 @@ function Search({ searchData, placeholder }) {
                   <p className={styles.albumTitle}>{option.title}</p>
 
                   <p className={styles.albumArtists}>
-                    {truncate(artists.join(", "), 40)}
+                    {/* {truncate(artists.join(", "), 40)} */}
                   </p>
                 </div>
               </li>
