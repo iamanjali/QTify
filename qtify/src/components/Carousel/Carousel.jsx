616f1daf6b408f04d9b1dd28d555880fec91b4b5
iamanjali <iamanjalijaiswal@gmail.com>
2024-09-01 22:02:07 +0530

    Navbar and Hero Section commit

--- qtify/src/components/Carousel/Carousel.jsx
+++ qtify/src/components/Carousel/Carousel.jsx
@@ -1,68 +1,24 @@
-// import React from 'react';
-// import { Swiper, SwiperSlide, Navigation } from 'swiper/react';
-// import 'swiper/css';
-// import 'swiper/css/bundle';
-
-// const Carousel = ({ items }) => {
-//   return (
-//     <div className="carousel-container">
-//       <Swiper
-//         modules={[Navigation]}
-//         spaceBetween={10}
-//         slidesPerView={3}
-//         navigation
-//         breakpoints={{
-//           640: { slidesPerView: 1 },
-//           768: { slidesPerView: 2 },
-//           1024: { slidesPerView: 3 },
-//         }}
-//       >
-//         {items.map(item => (
-//           <SwiperSlide key={item.id}>
-//             <div className="card-wrapper">
-//               {item.component}
-//             </div>
-//           </SwiperSlide>
-//         ))}
-//       </Swiper>
-//     </div>
-//   );
-// };
-
-// export default Carousel;
-
-import React, { useRef } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css';
-import 'swiper/css';
-import 'swiper/css/bundle';
-import { Pagination } from 'swiper/modules/pagination';
+import 'swiper/swiper-bundle.min.css'; // Update the path if necessary
+import { Navigation } from 'swiper';
+import styles from './Carousel.module.css' 
 
 const Carousel = ({ items }) => {
-  const swiperRef = useRef(null);
-
-  const handleSlidePrev = () => {
-    if (swiperRef.current) {
-      swiperRef.current.slidePrev();
-    }
-  };
-
-  const handleSlideNext = () => {
-    if (swiperRef.current) {
-      swiperRef.current.slideNext();
-    }
-  };
-
   return (
     <div className="carousel-container">
       <Swiper
-        modules={[Pagination]} // Include Pagination in the modules array
+        modules={[Navigation]}
         spaceBetween={10}
         slidesPerView={3}
-        pagination={{ clickable: true }} // Configure pagination options
-        onSwiper={(swiper) => { swiperRef.current = swiper; }}
+        navigation
+        breakpoints={{
+          640: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+        }}
       >
-        {items.map((item) => (
+        {items.map(item => (
           <SwiperSlide key={item.id}>
             <div className="card-wrapper">
               {item.component}
@@ -70,16 +26,61 @@ const Carousel = ({ items }) => {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className="custom-navigation">
-        <button className="navigation-button left" onClick={handleSlidePrev}>
-          {/* Left navigation button icon or text */}
-        </button>
-        <button className="navigation-button right" onClick={handleSlideNext}>
-          {/* Right navigation button icon or text */}
-        </button>
-      </div>
     </div>
   );
 };
 
 export default Carousel;
+
+// import React, { useRef } from 'react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/swiper-bundle.min.css';
+// import 'swiper/css';
+// import 'swiper/css/bundle';
+// import { Pagination } from 'swiper/modules/pagination';
+
+// const Carousel = ({ items }) => {
+//   const swiperRef = useRef(null);
+
+//   const handleSlidePrev = () => {
+//     if (swiperRef.current) {
+//       swiperRef.current.slidePrev();
+//     }
+//   };
+
+//   const handleSlideNext = () => {
+//     if (swiperRef.current) {
+//       swiperRef.current.slideNext();
+//     }
+//   };
+
+//   return (
+//     <div className="carousel-container">
+//       <Swiper
+//         modules={[Pagination]} // Include Pagination in the modules array
+//         spaceBetween={10}
+//         slidesPerView={3}
+//         pagination={{ clickable: true }} // Configure pagination options
+//         onSwiper={(swiper) => { swiperRef.current = swiper; }}
+//       >
+//         {items.map((item) => (
+//           <SwiperSlide key={item.id}>
+//             <div className="card-wrapper">
+//               {item.component}
+//             </div>
+//           </SwiperSlide>
+//         ))}
+//       </Swiper>
+//       <div className="custom-navigation">
+//         <button className="navigation-button left" onClick={handleSlidePrev}>
+//           {/* Left navigation button icon or text */}
+//         </button>
+//         <button className="navigation-button right" onClick={handleSlideNext}>
+//           {/* Right navigation button icon or text */}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Carousel;
